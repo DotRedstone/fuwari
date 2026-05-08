abdbc0c71b2aa2fd0a3d9e1817b0a5b94cf65f73
Dot <dot@example.com>
2026-05-08 23:40:01 +0800

    chore(config): 切换至根域名 bdot.in

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://your-blog-link.pages.dev/",
+	site: "https://bdot.in/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
@@ -98,7 +98,7 @@ export default defineConfig({
 				showCopyToClipboardButton: false,
 			}
 		}),
-        svelte(),
+		svelte(),
 		sitemap(),
 	],
 	markdown: {
